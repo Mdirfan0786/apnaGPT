ea6c0cc60b3ee9c387cf958edd2babd07165385d
Md Irfan <rn555666777@gmail.com>
2026-02-07 21:25:44 +0530

    ready for deployment

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,14 @@ import userRoutes from "./routes/user.routes.js";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(
+  cors({
+    origin: [process.env.LOCAL_URL, process.env.FRONTEND_URL],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+  }),
+);
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
