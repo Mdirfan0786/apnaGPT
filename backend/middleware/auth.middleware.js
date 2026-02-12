87e579fe906502d0dae81dbc34e153cde68fef2c
Md Irfan <rn555666777@gmail.com>
2026-02-12 18:05:03 +0530

    fix User module problem while export

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -10,11 +10,9 @@ export const isLoggedIn = async (req, res, next) => {
     }
 
     const token = authHeader.split(" ")[1];
-
     const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
 
     const user = await User.findById(decoded.id).select("-password");
-
     if (!user) {
       return res.status(401).json({ message: "User not found" });
     }
