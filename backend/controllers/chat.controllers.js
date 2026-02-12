39fd95d37ce580d892fde983363db9d843b49246
Md Irfan <rn555666777@gmail.com>
2026-02-12 17:54:33 +0530

    ready for deployment

--- backend/controllers/chat.controllers.js
+++ backend/controllers/chat.controllers.js
@@ -1,5 +1,4 @@
 import Thread from "../models/Thread.model.js";
-import { v4 as uuidv4 } from "uuid";
 import getOpenAIAPIResponse from "../utils/openAi.utils.js";
 import User from "../models/user.model.js";
 
